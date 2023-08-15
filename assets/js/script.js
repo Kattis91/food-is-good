acc0deff074ecad3f268076c8c9ab12738a4cfa8
Kattis91 Durneva Svedmark <katya.svedmark@gmail.com>
2023-08-15 12:51:16 +0000

    Add quiz questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const content = [
+    
+    { 
+        question: "What is the name of the traditional Spanish saffron-spiced rice dish with chicken and seafood cooked in a large pan?",
+        a: "Risotto",
+        b: "Paella",
+        c: "Quesadilla",
+        correct: "b",
+    },
+    {
+        question: "Chili sin carne and chili con carne are very similar. Which ingredient is removed from chili sin carne?",
+        a: "Meat",
+        b: "Tomato",
+        c: "Beans",
+        correct: "a",
+    },
+    {
+        question: "Have you heard of famous Swedish meatballs? Which country do they actually come from?",
+        a: "Greece",
+        b: "Turkey",
+        c: "Iran",
+        correct: "b",
+    },
+    {
+        question: "What date is the international pizza day celebrated?",
+        a: "January the 1st",
+        b: "August the 8th",
+        c: "November the 23th",
+        correct: "a",
+    },
+    {
+        question: "Have you heard of 'blini'? What kind of dish is that?",
+        a: "Russian pancakes",
+        b: "Polish pierogies",
+        c: "Hungarian soup",
+        correct: "a",
+    },
+    {
+        question: "You are invited to a party. You receive aperitif. What is that?",
+        a: "Starter",
+        b: "Dessert",
+        c: "Drink",
+        correct: "c",
+    },
+];
